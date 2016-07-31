49b16382c5881d83660e48f6ea8491e15e01ba91
coral <izjpbd@gmail.com>
2016-07-31 14:09:10 +0000

    checkbox margine

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+                    
    validates :age, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 130}, on: :update
    validates :profile, presence: true, length: { minimum: 15, maximum: 145 }, on: :update
    validates :location, presence: true, length: {minimum: 7, maximum: 50 }, on: :update
   # validates :latitude, numericality: { greater_than_or_equal_to: -90,  less_than_or_equal_to: 90 }, on: :update
   has_secure_password
+  has_many :microposts
 end
